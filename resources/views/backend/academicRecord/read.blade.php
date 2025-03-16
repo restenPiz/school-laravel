a778304403d52c75f60e099cbd501f5ac7eb57b6
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-17 01:51:26 +0200

    Some updates in the application

--- resources/views/backend/academicRecord/read.blade.php
+++ resources/views/backend/academicRecord/read.blade.php
@@ -29,9 +29,11 @@
                     </div>
                     <div class="block text-gray-600 font-bold">
                         <div class="relative">
-                            <select name="month" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                                <option value="">--Select Year--</option>
-                                
+                            <select name="year" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                                <option value="">-- Select Year --</option>
+                                @for ($year = 2020; $year <= date('Y'); $year++)
+                                    <option value="{{ $year }}">{{ $year }}</option>
+                                @endfor
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -48,8 +50,12 @@
                     <div class="block text-gray-600 font-bold">
                         <div class="relative">
                             <select name="month" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                                <option value="">--Select Month--</option>
-                                
+                                <option value="">-- Select Month --</option>
+                                @foreach (range(1, 12) as $month)
+                                    <option value="{{ str_pad($month, 2, '0', STR_PAD_LEFT) }}">
+                                        {{ date('F', mktime(0, 0, 0, $month, 1)) }}
+                                    </option>
+                                @endforeach
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
