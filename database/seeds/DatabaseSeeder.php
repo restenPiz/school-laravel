e1486558e8f580622adf5576e9fa16091f0f7cc8
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-16 22:43:18 +0200

    Some updates in the application

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,11 +8,6 @@ use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
         $this->call(RolesAndPermissionsSeeder::class);
