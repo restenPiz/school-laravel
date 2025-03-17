676f19b386e61b19ddf070a5fcf4b8f3a3893eda
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-17 18:30:36 +0200

    Some updates in the application

--- app/fees.php
+++ app/fees.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class fees extends Model
@@ -17,6 +18,25 @@ class fees extends Model
         'amount',
         'status'
     ];
+
+    public function getCoveredMonths()
+    {
+        $startMonth = Carbon::parse($this->due_date)->month;
+
+        switch ($this->payment_type) {
+            case 'monthly':
+                return [$startMonth]; // Apenas o mês da data de vencimento
+
+            case 'quartely':
+                return [$startMonth, $startMonth + 1, $startMonth + 2]; // Três meses
+
+            case 'yearly':
+                return range(1, 12); // Todos os 12 meses
+
+            default:
+                return [];
+        }
+    }
 
     public function student()
     {
