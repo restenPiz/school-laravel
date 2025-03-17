5a5b5d20adfb07749aaf2cec3c120e01533ba3fe
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-17 12:27:23 +0200

    Some updates in the app

--- app/fees.php
+++ app/fees.php
@@ -8,7 +8,7 @@ class fees extends Model
 {
     protected $table = 'fees';
 
-    protected $fillable = ['student_id', 'amount', 'due_date', 'payment_type', 'status'];
+    protected $fillable = ['student_id', 'amount', 'year', 'due_date', 'payment_type', 'status'];
 
     public function student()
     {
