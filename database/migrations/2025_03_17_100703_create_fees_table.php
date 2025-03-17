e2e3c6efce106a948a8e53669d7a80768c90f232
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-18 00:13:24 +0200

    Doing some updates in the application

--- database/migrations/2025_03_17_100703_create_fees_table.php
+++ database/migrations/2025_03_17_100703_create_fees_table.php
@@ -10,14 +10,19 @@ class CreateFeesTable extends Migration
     {
         Schema::create('fees', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('class_id');
             $table->enum('payment_type', ['monthly', 'quartely', 'yearly']);
-            $table->integer('year');
+            $table->decimal('amount_due', 10, 2);
+            $table->decimal('amount_paid', 10, 2)->default(0);
+            $table->decimal('penalty_fee', 10, 2)->default(0);
             $table->date('due_date');
-            $table->foreignId('class_id')->constrained('grades')->onDelete('cascade');
-            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
-            $table->decimal('amount', 10, 2); // Valor da propina
-            $table->enum('status', ['pending', 'paid'])->default('pending'); // Estado da propina
+            $table->enum('status', ['Pendente', 'Atrasado', 'Pago'])->default('Pendente');
             $table->timestamps();
+
+            // Chaves estrangeiras
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
         });
     }
 
