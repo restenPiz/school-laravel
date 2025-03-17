75d67996786dc1e0c86fafa1dd10a544e7359e59
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-17 02:18:30 +0200

    Some updates in the application

--- app/Http/Controllers/AcademicRecordController.php
+++ app/Http/Controllers/AcademicRecordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Grade;
 use App\Student;
 use Illuminate\Http\Request;
 
@@ -10,13 +11,15 @@ class AcademicRecordController extends Controller
     public function index()
     {
         $students = Student::all();
-        return view('backend.academicRecord.read', compact('students'));
+        $classes = Grade::all();
+        return view('backend.academicRecord.read', compact('students', 'classes'));
     }
 
     public function create()
     {
         $students = Student::all();
-        return view('backend.academicRecord.create', compact('students'));
+        $classes = Grade::all();
+        return view('backend.academicRecord.create', compact('students', 'classes'));
     }
 
     public function store(Request $request)
---
+++ resources/views/backend/academicRecord/create.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="roles">
+
+        <div class="flex items-center justify-between mb-6">
+            <div>
+                <h2 class="text-gray-700 uppercase font-bold">Add New Academic Record</h2>
+            </div>
+            <div class="flex flex-wrap items-center">
+                <a href="{{ route('parents.index') }}" class="bg-gray-700 text-white text-sm uppercase py-2 px-4 flex items-center rounded">
+                    <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"></path></svg>
+                    <span class="ml-2 text-xs font-semibold">Back</span>
+                </a>
+            </div>
+        </div>
+        <!-- Log on to codeastro.com for more projects -->
+        <div class="table w-full mt-8 bg-white rounded">
+            <form action="{{ route('parents.store') }}" method="POST" class="w-full max-w-xl px-6 py-12" enctype="multipart/form-data">
+                @csrf
+
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Payment Type
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <select name="month" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="">--Select Payment Type--</option>
+                            <option value="1">Monthly</option> 
+                            <option value="2">Quarterly</option>
+                            <option value="3">Annual</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Year
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <select name="year" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="">-- Select Year --</option>
+                            @for ($year = 2020; $year <= date('Y'); $year++)
+                                <option value="{{ $year }}">{{ $year }}</option>
+                            @endfor
+                        </select>
+                    </div>
+                </div>
+                
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Month
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <select name="month" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="">-- Select Month --</option>
+                            @foreach (range(2, 12) as $month)
+                                <option value="{{ str_pad($month, 2, '0', STR_PAD_LEFT) }}">
+                                    {{ date('F', mktime(0, 0, 0, $month, 1)) }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Classes
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <select type="hidden" name="month" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="">--Select Their Class--</option>
+                            @foreach ($classes as $class)
+                                <option value="{{ $class->id }}">{{$class->class_name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <!-- Log on to codeastro.com for more projects -->
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                            Student
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <select type="hidden" name="month" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="">--Select Student--</option>
+                            @foreach ($students as $student)
+                                <option value="{{ $student->id }}">{{$student->user->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <div class="md:flex md:items-center">
+                    <div class="md:w-1/3"></div>
+                    <div class="md:w-2/3">
+                        <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </form>        
+        </div>
+        <!-- Log on to codeastro.com for more projects -->
+    </div>
+@endsection
