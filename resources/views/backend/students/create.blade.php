5e6f556849a8d0eeeacc628161052f8212b473cd
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-18 18:17:07 +0200

    Some updates in the application

--- resources/views/backend/students/create.blade.php
+++ resources/views/backend/students/create.blade.php
@@ -199,6 +199,20 @@
                         <input name="profile_picture" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="file">
                     </div>
                 </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Payment Type
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <select name="payment_type" class="block font-bold appearance-none w-full bg-gray-200 border border-gray-200 text-gray-600 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option value="monthly">Mensal</option>
+                            <option value="quartely">Trimestral</option>
+                            <option value="yearly">Anual</option>
+                        </select>
+                    </div>
+                </div>
 
                 <div class="md:flex md:items-center">
                     <div class="md:w-1/3"></div>
