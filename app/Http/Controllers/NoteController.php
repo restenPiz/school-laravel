45492112ffb56eff79cee3479cbf1596fbcd16fe
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-22 14:52:01 +0200

    Some updates in the application

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Grade;
+use App\Note;
+use App\Student;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
     public function index()
     {
+        $students = Student::all();
+        $classes = Grade::all();
 
+        return view('backend.notes.index', compact('students', 'classes'));
+    }
+    public function store(Request $request)
+    {
+        $notes = new Note();
+
+        $notes->note = $request->input('note');
+        $notes->type = $request->input('type');
+        $notes->subject_id = $request->input('subject_id');
+        $notes->student_id = $request->input('student_id');
+        $notes->save();
+
+        return redirect()->back();
     }
 }
