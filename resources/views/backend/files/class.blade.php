08a928167c948127798ff92256017fc3b87eeeed
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-31 15:44:30 +0200

    Some updates in the app

--- resources/views/backend/files/class.blade.php
+++ resources/views/backend/files/class.blade.php
@@ -1,20 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    </link>
     <div class="roles">
 
         {{--?Start with the main content--}}
-        <div class="w-full block mt-8">
-            <div class="flex flex-wrap sm:flex-no-wrap justify-between">
-                <div class="w-full sm:w-1/2 mr-2 mb-6">
+        <div class="max-w-8xl mx-auto">
+            <div class="flex items-center justify-between mb-6">
+                <div class="{{--w-full sm:w-1/2 mr-2 mb-6--}}">
                     <h3 class="text-gray-700 uppercase font-bold mb-2">Subject List 
                     </h3>
-                    <div class="flex flex-wrap items-center">
+                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-2">
                         @foreach ($teacher->subjects as $class)
-                            <div class="w-full sm:w-1/2 text-center border border-gray-400 rounded">
+                            <div class="flex flex-col h-full bg-white p-6 rounded-lg shadow-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                                 <div class="text-gray-800 uppercase font-semibold px-4 py-4 mb-2">{{ $class->name }}</div>
                                 <a href="{{ route('files',$class->id) }}" 
-                                    class="bg-green-600 inline-block mb-4 text-xs text-white uppercase font-semibold px-4 py-2 border border-gray-200 rounded">Manage Files</a>
+                                    class="text-center bg-green-600 inline-block mb-4 text-xs text-white uppercase font-semibold px-4 py-2 border border-gray-200 rounded">Manage Files</a>
                             </div>
                         @endforeach
                     </div>
--- resources/views/backend/files/studentFile.blade.php
+++ resources/views/backend/files/studentFile.blade.php
@@ -10,10 +10,10 @@
         <div class="max-w-8xl mx-auto">
             <div class="flex items-center justify-between mb-6">
                 <div>
-                   <h2 class="text-2xl font-semibold">Arquivos da Disciplina - {{$class->name}}</h2>
+                    <h2 class="text-2xl font-semibold">Arquivos da Disciplina - {{$class->name}}</h2>
                 </div>
                 <div class="flex flex-wrap items-center">
-                    <a href="{{route('studentClasses',Auth::user()->id)}}" class="bg-gray-700 text-white text-sm uppercase py-2 px-4 flex items-center rounded">
+                    <a href="{{route('studentClasses', Auth::user()->id)}}" class="bg-gray-700 text-white text-sm uppercase py-2 px-4 flex items-center rounded">
                         <svg class="w-3 h-3 fill-current" aria-hidden="true" focusable="false" data-prefix="fas"
                             data-icon="long-arrow-alt-left" class="svg-inline--fa fa-long-arrow-alt-left fa-w-14" role="img"
                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
@@ -25,36 +25,43 @@
                 </div>
             </div>
         
-            {{-- Use grid responsivo --}}
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-2">
-                @foreach ($class->files as $file)
-                    <div class="flex flex-col h-full bg-white p-6 rounded-lg shadow-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#" class="flex-shrink-0">
-                            @if(in_array(pathinfo($file->file_path, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png', 'gif', 'bmp']))
-                                <img class="rounded-t-lg" src="{{ asset('storage/' . $file->file_path) }}" alt="Image" />
-                            @elseif(in_array(pathinfo($file->file_path, PATHINFO_EXTENSION), ['docx']))
-                                <img class="rounded-t-lg" src="../images/docx.png" alt="PDF" />
-                            @else
-                                <img class="rounded-t-lg" src="../images/pdf.png" alt="PDF" />
-                            @endif
-                        </a>
-                        <div class="flex-1 p-5">
-                            <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $file->title }}</h5>
+            {{-- Verifica se há arquivos ou exibe uma mensagem informando que não há --}}
+            @if($class->files->isEmpty())
+                <div class="text-center p-4">
+                    <p class="text-lg text-gray-500">Não há arquivos disponíveis para esta disciplina.</p>
+                </div>
+            @else
+                {{-- Use grid responsivo --}}
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 mb-2">
+                    @foreach ($class->files as $file)
+                        <div class="flex flex-col h-full bg-white p-6 rounded-lg shadow-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#" class="flex-shrink-0">
+                                @if(in_array(pathinfo($file->file_path, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png', 'gif', 'bmp']))
+                                    <img class="rounded-t-lg" src="{{ asset('storage/' . $file->file_path) }}" alt="Image" />
+                                @elseif(in_array(pathinfo($file->file_path, PATHINFO_EXTENSION), ['docx']))
+                                    <img class="rounded-t-lg" src="../images/docx.png" alt="PDF" />
+                                @else
+                                    <img class="rounded-t-lg" src="../images/pdf.png" alt="PDF" />
+                                @endif
                             </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400" style="text-align: justify;">{{ $file->description }}</p>
-                            <div class="flex space-x-2 flex-wrap justify-start">
-                                {{-- Action buttons for file --}}
-                                <a href="{{ asset('storage/' . $file->file_path) }}" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    <svg class="rtl:rotate-90 w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                    </svg>
+                            <div class="flex-1 p-5">
+                                <a href="#">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $file->title }}</h5>
                                 </a>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400" style="text-align: justify;">{{ $file->description }}</p>
+                                <div class="flex space-x-2 flex-wrap justify-start">
+                                    {{-- Action buttons for file --}}
+                                    <a href="{{ asset('storage/' . $file->file_path) }}" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        <svg class="rtl:rotate-90 w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                        </svg>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
+                    @endforeach
+                </div>
+            @endif
         </div>
         {{-- End of main content --}}
 
